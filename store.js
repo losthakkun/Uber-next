83b74c97228c9b385f623dd5f02a91f2de9bc398
losthakkun <losthakkun@gmail.com>
2021-12-05 19:30:54 -0600

    Add HomeScreen component

--- store.js
+++ store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navReducer from "./slices/navSlice";
 
+//? Create the store and export it
 export const store = configureStore({
+	//? Add the reducers to the store
 	reducer: {
 		nav: navReducer,
 	},

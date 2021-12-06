be3586caf82702597b0dfde17ae4b3d1c531c89a
losthakkun <losthakkun@gmail.com>
2021-12-05 19:13:35 -0600

    Setting up Redux navReducer

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build UBER!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets build UBER!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 

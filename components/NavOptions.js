01538d91b3d5c4602f961173e14107bddb2607f1
losthakkun <losthakkun@gmail.com>
2021-12-11 19:02:05 -0600

    Setting up navigation stacks

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 
+import { useNavigation } from '@react-navigation/core'
+import { Icon } from 'react-native-elements'
+
 import tw from 'twrnc'
 
 //? Define the NavOptions list
@@ -10,7 +13,7 @@ const data = [
 		id    : "1",
 		title : "Viajar",
 		image : "https://links.papareact.com/3pn",
-		screen: "MapsScreen",
+		screen: "MapScreen",
 	},
 	{
 		id    : "2",
@@ -22,21 +25,29 @@ const data = [
 
 //? Define the NavOptions component
 const NavOptions = () => {
+	const navigation = useNavigation();
 	return (
 		<FlatList
 			data       = {data}
 			horizontal = {true}
 			renderItem = {({ item }) => (
 				<TouchableOpacity
+					onPress = {() => navigation.navigate(item.screen)}
 					style = {tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
 				>
 					<View>
 						<Image
-							source={{ uri: item.image }}
-							style={{ width: 120, height: 120, resizeMode: "contain" }}
+							source = {{ uri: item.image }}
+							style  = {{ width: 120, height: 120, resizeMode: "contain" }}
 						/>
 					</View>
-					<Text style={tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
+					<Text style = {tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
+					<Icon
+						style = {tw `p-2 bg-black rounded-full w-10 mt-4`}
+						name  = "arrowright"
+						color = "white"
+						type  = "antdesign"
+					/>
 				</TouchableOpacity>
 			)}
 			keyExtractor = {item => item.id}
